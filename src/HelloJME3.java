0d91563b8e8d363b552ed305fccad775760540ca
Yufei Zheng <yufei@noname.img.is-private>
2018-09-21 18:20:20 +0900

    Initial commit

---
+++ src/HelloJME3.java
@@ -0,0 +1,22 @@
+//package net.jmecn;
+
+import com.jme3.app.SimpleApplication;
+
+/**
+ * 你的第一个jME3程序
+ * @author yanmaoyuan
+ */
+public class HelloJME3 extends SimpleApplication {
+
+    @Override
+    public void simpleInitApp() {
+        // TODO 初始化场景
+    }
+
+    public static void main(String[] args) {
+        // 启动jME3程序
+        HelloJME3 app = new HelloJME3();
+        app.start();
+    }
+
+}
